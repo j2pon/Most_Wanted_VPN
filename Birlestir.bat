389f68c59e170430a6e2e74c613bdd217c89b13a
j2pon <j2pon@users.noreply.github.com>
2026-09-21 20:21:57 +0300

    v1.2: Fix BMW M3 GTR color in GlobalB & add MWCrashFix.asi for pursuit R6025 crash

---
+++ Birlestir.bat
@@ -0,0 +1,14 @@
+@echo off
+title NFS Most Wanted VPN Edition - Parca Birlestirici (v1.2)
+cd /d "%~dp0"
+echo ====================================================================
+echo  Need for Speed: Most Wanted - VPN Edition v1.2
+echo  Parcalar birlestiriliyor, lutfen bekleyin...
+echo ====================================================================
+copy /b NFS_MW_VPN_Edition_v1.2.part01.zip + NFS_MW_VPN_Edition_v1.2.part02.zip + NFS_MW_VPN_Edition_v1.2.part03.zip + NFS_MW_VPN_Edition_v1.2.part04.zip NFS_MW_VPN_Edition_v1.2.zip
+echo.
+echo [+] Birlestirme basariyla tamamlandi! 
+echo [+] Olusan 'NFS_MW_VPN_Edition_v1.2.zip' dosyasini saga tiklayip cikartabilirsiniz.
+pause
+
+
